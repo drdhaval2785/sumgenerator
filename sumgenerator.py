a2102a52ba5025968ab56aa092602fcbbf8294e1
drdhaval2785 <drdhaval2785@gmail.com>
2019-08-12 09:58:35 +0530

    increase the font size, add comments

--- sumgenerator.py
+++ sumgenerator.py
@@ -73,54 +73,88 @@ def create_subtraction(length, n, take=True):
 	# Return result.
 	return result
 
-def pretty_sum(digitTuples, arithmaticOperator, dttm, sumsInARow=10, writeAnswer=False):
+def pretty_sum(digitTuples, arithmaticOperator, dttm, sumsInARow=5, writeAnswer=False):
+	"""Pretty print the sums in html.
+
+	Show <sumsInARow> sums on a line.
+	"""
+	
+	# If Answers are to be printed (for parents / teachers), append ans to the filename.
 	if writeAnswer:
 		fout = open('math_ans_'+dttm+'.html', 'w')
+	# Otherwise for kids, append que to filename.
 	else:
 		fout = open('math_que_'+dttm+'.html', 'w')
-	fout.write('<!DOCTYPE html>\n<html lang="en">\n<head>\n<meta charset="utf-8"/>\n<title>\nMath sum generator</title>\n<style>\ntable, td {  border: 2px solid black; padding: 10px;}\n td { text-align: right; }\n</style>\n</head>\n<body>\n<table>\n')
+	# Generic HTML and CSS starting.
+	fout.write('<!DOCTYPE html>\n<html lang="en">\n<head>\n<meta charset="utf-8"/>\n<title>\nMath sum generator</title>\n<style>\ntable, td {  border: 2px solid black; padding: 10px; font-size: 20px;}\n td { text-align: right; }\n</style>\n</head>\n<body>\n<table>\n')
+	# Initialize counter
 	counter = 0
+	# a and b are the numbers
 	for (a, b) in digit_tuples:
+		# At every sumsInARow, insert a new table row.
 		if counter % sumsInARow == 0:
 			startCounter = counter
 			fout.write('<tr>\n')
+		# Generate Sum.
 		summ = '<td>'
 		summ += '<br>' + a + '<br/>'
 		summ += arithmaticOperator + ' ' + b + '<br/>'
 		summ += '--------------' + '<br/>'
+		# If answer is to be written, do sum actually and print.
 		if writeAnswer:
 			if arithmaticOperator == '+':
 				summ += str(int(a) + int(b)) + '<br/>'
-			elif airthmaticOperator == '-':
+			elif arithmaticOperator == '-':
 				summ += str(int(a) - int(b)) + '<br/>'
+		# Else leave a blank line.
 		else:
 			summ += '<br/>'
-		summ += '</td>'			
+		# Close the td.
+		summ += '</td>'	
+		# Write summ to the file.
 		fout.write(summ)
+		# Close the row at specified interval i.e. <sumsInARow>.
 		if counter - startCounter == sumsInARow - 1:
 			fout.write('\n</tr>\n')
+		# Increment counter.
 		counter += 1
+	# Close HTML.
 	fout.write('</table>\n</body>\n</html>')
 
+
 if __name__ == "__main__":
+	# Operation can be + or -
 	operation = sys.argv[1]
+	# how many digits in the number?
 	digits = int(sys.argv[2])
+	# Default sums to generate are 25.
 	if len(sys.argv) > 3:
 		sumsToGenerate = int(sys.argv[3])
 	else:
-		sumsToGenerate = 100
+		sumsToGenerate = 25
+	# Default is to allow take / carry.
+	# If there is specific mention of 'notake', take / carry is not allowed.
 	if len(sys.argv) > 4:
 		take = sys.argv[4]
 	else:
 		take = 'take'
+	# Date time in string format.
 	dttm = date_time()
+	# If addition,
 	if operation == '+':
+		# Create sums
 		digit_tuples = create_sum(digits, sumsToGenerate, take)
+		# Write without answers to HTML.
 		pretty_sum(digit_tuples, '+', dttm)
+		# Write with answers to HTML.
 		pretty_sum(digit_tuples, '+', dttm, writeAnswer=True)
+	# If subtraction,
 	elif operation == '-':
+		# Create sums.
 		digit_tuples = create_subtraction(digits, sumsToGenerate, take)
+		# Write without answers to HTML.
 		pretty_sum(digit_tuples, '-', dttm)
+		# Write with answers to HTML.
 		pretty_sum(digit_tuples, '-', dttm, writeAnswer=True)
 
 
